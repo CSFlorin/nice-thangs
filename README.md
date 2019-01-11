ebbd9987f568c0b0201f0cb2f737a817d54b5f55
Florin Langer <florin@berkeley.edu>
2019-01-11 14:18:14 -0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+In my student cooperative at uni, I gave a platform for members to leave comments for each other at the end of the semester. With 42 people, it would have taken quite a while to compile these into a nice note for each person, so I made a quicker way to do it.
+
+1. Make a Google Form following [this template](https://docs.google.com/forms/d/e/1FAIpQLScE2oANQzqG4JZ_Jf6JZ01Zc6eP3knVI77MffDL0sqkJyRVLQ/viewform?usp=sf_link).
+
+2. Download the responses as a CSV.
+
+3. `python3 -m virtualenv env`
+
+4. `source env/bin/activate`
+
+5. `pip install -r requirements.txt`
+
+6. `python nice-thangs.py`
+
+7. Select all the files in `out/` and press `Command+P` to print them.
---
+++ nice-thangs.py
@@ -0,0 +1,28 @@
+from docxtpl import DocxTemplate, R
+import pandas as pd
+
+
+def format_name(name):
+    if ',' in name:
+        name = name.split(',')
+        name = list(map(lambda x: x.strip(), name))
+        name = ' '.join(name[::-1])
+    return name
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("nice-thangs.csv")
+    df = df.drop(['Timestamp'], axis=1)
+
+    for column in df:
+        thangs = df[column].dropna().sample(frac=1).values
+        thangs = '\t' + '\n\n'.join(thangs)  # for Python 2: .decode('utf-8')
+
+        doc = DocxTemplate("template.docx")
+        context = {
+            'name' : format_name(column),
+            'thangs': R(thangs)
+        }
+        doc.render(context)
+        doc.save("out/" + column + "2.docx")
+        exit()
